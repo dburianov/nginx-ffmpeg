f775d2e9c1bb0fd62237465a8ba07285f69a3d94
Dmytro Burianov <dmytro@burianov.net>
2020-02-24 00:11:57 +0200

    add consul

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-
+consul agent -data-dir=/etc/consul.d -config-dir=/etc/consul.d  -retry-join 10.239.110.194 --bootstrap-expect=1 &
 /usr/local/nginx/sbin/nginx -g 'daemon off;'
 /bin/bash
 

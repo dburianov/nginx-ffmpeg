bb9e581f727cb6976e4e6da6c602d5ea5499fc8a
Dmytro Burianov <dmytro@burianov.net>
2020-02-24 13:57:07 +0200

    correct consul for k8s

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #
-consul agent -data-dir=/etc/consul.d -config-dir=/etc/consul.d  -retry-join 10.239.110.194 --bootstrap-expect=1 &
+#consul agent -data-dir=/etc/consul.d -config-dir=/etc/consul.d  -retry-join 10.239.110.194 --bootstrap-expect=1 &
+consul agent -config-dir=/etc/consul.d  -retry-join 10.239.110.194 &
 /usr/local/nginx/sbin/nginx -g 'daemon off;'
 /bin/bash
 
